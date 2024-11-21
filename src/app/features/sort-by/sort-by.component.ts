88e62ed3472e83cf49aea76ea9f93653a5c8b9f1
Maximino Castillo <maxcastgarcia@gmail.com>
2024-11-21 00:56:38 -0600

    Adjust markup in the sort-by component

--- src/app/features/sort-by/sort-by.component.ts
+++ src/app/features/sort-by/sort-by.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sort-by',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sort-by.component.html',
   styleUrl: './sort-by.component.css'
 })
 export class SortByComponent {
+  sortColumn: string = 'date';
+  sortDirection: string = 'desc';
 
+  sortData(column: string): void {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+    this.sortArray();
+  }
+
+  sortArray(): void {
+    console.log(`Sorting by ${this.sortColumn} in ${this.sortDirection} order`);
+  }
 }
